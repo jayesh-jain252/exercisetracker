fbc978dc09c8214b1ad9d6c4f9dbaa669e3f7b6d
jay1234321 <jayesh64674@gmail.com>
2022-05-31 12:18:03 +0530

    added env file

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const moment = require("moment");
 const User = require("./models/user");
 const Log = require("./models/log");
 const Exercise = require("./models/exercise");
